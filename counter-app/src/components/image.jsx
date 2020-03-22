81d6fcecb747524c9ba36f60468276bd86994380
Yurie <yurie.koga.a@gmail.com>
2020-03-21 21:36:36 -0700

    update image.jsx

--- counter-app/src/components/image.jsx
+++ counter-app/src/components/image.jsx
@@ -3,15 +3,16 @@ import "./image.css";
 
 class Image extends Component {
   state = {
-    imageUrl: "https://picsum.photos/200"
+    // imageUrl: "https://picsum.photos/200"
   };
 
   render() {
     return (
       <div className="imgBox">
-        <img src={this.state.imageUrl} alt="" className="img" />
-        <img src={this.state.imageUrl} alt="" className="img" />
-        <img src={this.state.imageUrl} alt="" className="img" />
+        <img src="./img/tiger.png" alt="" className="img" />
+        <img src="./img/wolf.png" alt="" className="img" />
+        <img src="./img/bird.png" alt="" className="img" />
+        {/* <img src={this.state.imageUrl} alt="" className="img" /> */}
       </div>
     );
   }
--- counter-app/src/components/navbar.jsx
+++ counter-app/src/components/navbar.jsx
@@ -11,7 +11,7 @@ const NavBar = ({ totalCounters }) => {
   return (
     <nav className="navbar navbar-light bg-light">
       <a className="navbar-brand" href="#">
-        Navbar{" "}
+        Count of incremented elements{" "}
         <span className="badge badge-pill badge-secondary">
           {/* {this.props.totalCounters} */}
           {/* {props.totalCounters} */} {/* destruct 'props' */}
